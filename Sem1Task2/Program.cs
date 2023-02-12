4f1afa6e7c2cf95064d8665dd2cb80d09de2e366
Elena <lenchikpim@mail.ru>
2023-02-12 16:35:56 +0300

    задачи 2,4,6,8

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача №2 
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1>number2)
+{
+  Console.WriteLine("Число А>B");
+}
+else
+{
+  Console.WriteLine("Число B>A");
+}
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача №4 
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1>number2)
+{
+  if (number1>number3)
+  {
+    Console.WriteLine("Максимальное число: "+number1);
+  }
+  else
+  {
+    Console.WriteLine("Максимальное число: "+number3);
+  }
+}
+else
+{
+  if ( number2>number3)
+  {
+    Console.WriteLine("Максимальное число: "+number2);
+  }
+  else
+  {
+    Console.WriteLine("Максимальное число: "+number3);
+  }
+}
