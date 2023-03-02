71b484165b3aa2eb12261d1872138773e9da6e7b
Elena <Lenchikpim@mail.ru>
2023-03-02 12:57:35 +0300

    Вариант преподавателя 34,38

--- Sem5Task34/Program.cs
+++ Sem5Task34/Program.cs
@@ -1,26 +1,84 @@
 ﻿// // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
 // // которая покажет количество чётных чисел в массиве.
+// int[] Gen1DArr(int len, int min, int max)
+// {
+//     // Корректировка входных данных
+//     if (min > max)
+//     {
+//         int buf = min;
+//         min = max;
+//         max = buf;
+//     }
+
+//     Random rnd = new Random();
+//     int[] arr = new int[len];
+
+//     for (int i = 0; i < len; i++)
+//     {
+//         arr[i] = rnd.Next(min, max + 1);
+//     }
+
+//     return arr;
+// }
+
+// void Print1DArr(int[] arr)
+// {
+//     Console.Write("[");
+
+//     for (int i = 0; i < arr.Length - 1; i++)
+//     {
+//         Console.Write($"{arr[i]},\t");
+//     }
+//     Console.WriteLine($"{arr[arr.Length - 1]}]");
+// }
+// //Метод для вывода данных
+// void WriteMess(string mess)
+// {
+//     Console.WriteLine(mess);
+// }
+// // Метод подсчета
+// int NumElemInRange(int[] arr)
+// {
+//     int res = 0;
+
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] % 2 == 0) res++;
+//     }
+
+//     return res;
+// }
+
+
+
+// int[] mass = Gen1DArr(11, 100, 1000);
+// Print1DArr(mass);
+
+
+// WriteMess($"Четных чисел в массиве: {NumElemInRange(mass)}");
+
+
+
+// Вариант преподавателя
+
+// Метод генерирующий массив
 int[] Gen1DArr(int len, int min, int max)
 {
-    // Корректировка входных данных
-    if (min > max)
-    {
-        int buf = min;
-        min = max;
-        max = buf;
-    }
-
-    Random rnd = new Random();
     int[] arr = new int[len];
 
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = rnd.Next(min, max + 1);
+        arr[i] = new Random().Next(min, max + 1);
     }
 
     return arr;
 }
-
+// Метод, выводящий на печать данные пользователя
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+// Метод выводящий на печать массив
 void Print1DArr(int[] arr)
 {
     Console.Write("[");
@@ -30,11 +88,6 @@ void Print1DArr(int[] arr)
         Console.Write($"{arr[i]},\t");
     }
     Console.WriteLine($"{arr[arr.Length - 1]}]");
-}
-//Метод для вывода данных
-void WriteMess(string mess)
-{
-    Console.WriteLine(mess);
 }
 // Метод подсчета
 int NumElemInRange(int[] arr)
@@ -48,12 +101,51 @@ int NumElemInRange(int[] arr)
 
     return res;
 }
+// Метод сортировки пузырьком
+void BubbleSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    for (int j = 0; j < arr.Length - i - 1; j++)
+     {
+        if (arr[j] > arr[j + 1])
+        {
+            int buf=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=buf;
+        }
+     }           
+}
+// Сортировка методом подсчета
+void CountingSort(int[] arr)
+{
+    int max=arr.Max();
+    int min=arr.Min();
 
+    int[] count=new int[max-min+1];
+    int z =0;
+    for(int i=0;i<count.Length;i++)
+    {
+        count[i]=0;
+    }
+    for (int i=0;i< arr.Length;i++)
+    {
+        count[arr[i]-min]++;
+    }
+    for(int i =min;i<=max;i++)
+    {
+        while(count[i-min]-->0)
+        {
+            arr[z]=1;
+            z++;
+        }
+    }
 
+}
 
-int[] mass = Gen1DArr(11, 100, 1000);
+int[] mass = Gen1DArr(115, 100, 1000);
+// BubbleSort(mass);
 Print1DArr(mass);
+int result=NumElemInRange(mass);
+PrintData(result.ToString());
 
-
-WriteMess($"Четных чисел в массиве: {NumElemInRange(mass)}");
-
+CountingSort(mass);
--- Sem5Task38/Program.cs
+++ Sem5Task38/Program.cs
@@ -1,90 +1,132 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
-void PrintData(string text, double print)
+// void PrintData(string text, double print)
+// {
+//     Console.WriteLine($"{text}:{print}");
+// }
+// double[] GenArry(int min, int max, int indexcount)
+// {
+//     // Метод генерирования
+//     int buf;
+//     if (min > max)
+//     {
+//         buf = min;
+//         min = max;
+//         max = buf;
+//     }
+//     double[] binarray = new double[indexcount];
+//     System.Random numSintezator = new System.Random();
+//     for (int i = 0; i < indexcount; i++)
+//     {
+//       binarray[i] = Math.Round(-min + numSintezator.NextDouble()*(min+max),2);
+//     }
+//     return binarray;
+// }
+// // Вывод массива на консоль
+// void Print1DArr(double[] arr)
+// {
+//     Console.Write("[");
+
+//     for (int i = 0; i < arr.Length - 1; i++)
+//     {
+//         Console.Write($"{arr[i]}, ");
+//     }
+//     Console.WriteLine($"{arr[arr.Length - 1]}]");
+// }
+// // Поиск пар чисел
+// (double min, double max) FindMinMaxArry(double[] array)
+// {
+//     double arrlen = array.Length;
+//     double min = array[0];
+//     double max = array[0];
+//     for (int i = 1; i < arrlen; i++)
+//     {
+//         if (array[i] < min)
+//         {
+//             min = array[i];
+//         }
+//         else
+//         {
+//             max = array[i];
+//         }
+
+//     }
+
+//     return (min, max);
+// }
+// //Сортируем
+// void SortInsertArry(double[] array)
+// { 
+
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//         double k = array[i];
+//         int j = i - 1;
+
+//         while (j >= 0 && array[j] > k)
+//         {
+//             array[j + 1] = array[j];
+//             j--;
+//         }
+//         array[j + 1] = k;
+//     }
+
+// }
+// //Вывод исходного массива
+// double[] array = GenArry(1, 100, 15);
+// Console.Write("Исходный массив:");
+// Print1DArr(array);
+// Console.Write("Сортировка методом вставки:");
+// SortInsertArry(array);
+// Print1DArr(array);
+
+// (double min, double max) result = FindMinMaxArry(array);
+// PrintData("Минимальное значение", result.min);
+// PrintData("Максимальное значение", result.max);
+// PrintData("Разница max-min", result.max - result.min);
+
+// Вариант преподователя
+// Метод генерирующий массив
+double[] Gen1DArr(int len, int min, int max)
 {
-    Console.WriteLine($"{text}:{print}");
-}
-double[] GenArry(int min, int max, int indexcount)
-{
-    // Метод генерирования
-    int buf;
-    if (min > max)
+    double[] arr = new double[len];
+
+    for (int i = 0; i < arr.Length; i++)
     {
-        buf = min;
-        min = max;
-        max = buf;
+        arr[i] = new Random().Next(min, max + 1)+new Random().NextDouble();
+        arr[i]=Math.Round(arr[i],4);
     }
-    double[] binarray = new double[indexcount];
-    System.Random numSintezator = new System.Random();
-    for (int i = 0; i < indexcount; i++)
-    {
-      binarray[i] = Math.Round(-min + numSintezator.NextDouble()*(min+max),2);
-    }
-    return binarray;
+
+    return arr;
 }
-// Вывод массива на консоль
+// Метод выводящий на печать массив
 void Print1DArr(double[] arr)
 {
     Console.Write("[");
 
     for (int i = 0; i < arr.Length - 1; i++)
     {
-        Console.Write($"{arr[i]}, ");
+        Console.Write($"{arr[i]},\t");
     }
     Console.WriteLine($"{arr[arr.Length - 1]}]");
 }
-// Поиск пар чисел
-(double min, double max) FindMinMaxArry(double[] array)
+// Метод сортировки пузырьком
+void BubbleSort(double[] arr)
 {
-    double arrlen = array.Length;
-    double min = array[0];
-    double max = array[0];
-    for (int i = 1; i < arrlen; i++)
-    {
-        if (array[i] < min)
+    for (int i = 0; i < arr.Length; i++)
+    for (int j = 0; j < arr.Length - i - 1; j++)
+     {
+        if (arr[j] > arr[j + 1])
         {
-            min = array[i];
+            double buf=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=buf;
         }
-        else
-        {
-            max = array[i];
-        }
-
-    }
-
-    return (min, max);
+     }           
 }
-//Сортируем
-void SortInsertArry(double[] array)
-{ 
-
-    for (int i = 1; i < array.Length; i++)
-    {
-        double k = array[i];
-        int j = i - 1;
-
-        while (j >= 0 && array[j] > k)
-        {
-            array[j + 1] = array[j];
-            j--;
-        }
-        array[j + 1] = k;
-    }
-
-}
-//Вывод исходного массива
-double[] array = GenArry(1, 100, 15);
-Console.Write("Исходный массив:");
+double[] array = Gen1DArr(123, 10, 150);
 Print1DArr(array);
-Console.Write("Сортировка методом вставки:");
-SortInsertArry(array);
+BubbleSort(array);
 Print1DArr(array);
-
-(double min, double max) result = FindMinMaxArry(array);
-PrintData("Минимальное значение", result.min);
-PrintData("Максимальное значение", result.max);
-PrintData("Разница max-min", result.max - result.min);
-
-
-
+Console.WriteLine(array[array.Length-1]-array[0]);
 
